6bf2e43a5f68d38d8d73c644a894b8296ee9f53e
reinoldocaceres <reinoldodcaceres@gmail.com>
2025-07-15 22:50:34 -0400

    Fix path-to-regexp error in server.js

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.get('/health', (req, res) => {
 });
 
 // Handle React routing, return all requests to React app
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
