56be517ac5d3ed170cdb72c3551c6919343025eb
reinoldocaceres <reinoldodcaceres@gmail.com>
2025-07-20 23:48:49 -0400

    change keys on .env

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -15,7 +15,7 @@ const Hero: React.FC = () => {
       <div className="hero__content">
         {/* Main Brand Display */}
         <div className="hero__brand">
-          <h1 className="hero__title">russtikkgi</h1>
+          <h1 className="hero__title">Russtikk</h1>
           <p className="hero__scroll">Scroll down</p>
         </div>
 
